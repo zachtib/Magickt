1d61ed27ee91c78e4208d05cc79808aaad5d7392
Zach Tibbitts <zachtib@gmail.com>
2021-06-03 15:03:16 -0400

    Exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+
 
 plugins {
     application
@@ -32,5 +34,10 @@ dependencies {
     implementation("io.ktor:ktor-websockets:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
